af01f8c1aa640902bce560387a1f84708455b679
Ilyas Ronef <ilyas@divandesign.biz>
2014-03-13 10:06:27 +0600

    Version 3.2: + Добавлен параметр «$orderBy», позволяющий задать сортировку элементов по строкам (слева направо → сверху вниз). * Исправлена ошибка с «$rowsMin» (переменная там не использовалась, получалось, что параметр бесполезен). * Рефакторинг.

--- ddGetColumnData.php
+++ ddGetColumnData.php
@@ -1,16 +1,17 @@
 <?php
 /**
  * ddGetColumnData.php
- * @version 3.1 (2013-03-22)
+ * @version 3.2 (2013-07-11)
  * 
  * @desc Выводит результаты Ditto в несколько колонок, стараясь равномерно распределить количество.
  * 
- * @notes Сниппет берёт результаты Ditto из плэйсхолдера, так что перед его вызовом необходимо вызывать Ditto с параметром «save» == 3.
+ * @note Сниппет берёт результаты Ditto из плэйсхолдера, так что перед его вызовом необходимо вызывать Ditto с параметром «save» == 3.
  * 
  * @uses Сниппет Ditto 2.1.
  * 
  * @param $columnsNumber {integer} - Количество колонок. Default: 1.
  * @param $rowsMin {integer} - Минимальное количество строк в одной колонке (0 — любое). Default: 0;
+ * @param $orderBy {'column'; 'row'} - Порядок элементов: 'column' - сначала заполняется первая колонка, потом вторая и т.д. ([[1, 2, 3] [4, 5, 6] [7, 8, 9]]); 'row' - элементы располагаются по срокам ([[1, 4, 7] [2, 5, 8] [3, 6, 9]]). Default: 'column'.
  * @param $columnTpl {string} - Шаблон колонки. Доступные плэйсхолдеры: [+wrapper+]. @required
  * @param $columnLastTpl {string} - Шаблон последней колонки. Доступные плэйсхолдеры: [+wrapper+]. Default: = $columnTpl.
  * @param $dittoId {integer} - Унакальный ID сессии Ditto. Default: ''.
@@ -20,9 +21,11 @@
  */
 
 //Количество колонок
-$columnsNumber = isset($columnsNumber) ? $columnsNumber : 1;
+$columnsNumber = (isset($columnsNumber) && is_numeric($columnsNumber) && $columnsNumber > 0) ? $columnsNumber : 1;
 //Минимальное количество строк
 $rowsMin = isset($rowsMin) ? intval($rowsMin) : 0;
+//Сортировка между колонками
+$orderBy = isset($orderBy) ? $orderBy : 'column';
 //Если шаблон последней колонки, не задан — будет как и все
 $columnLastTpl = isset($columnLastTpl) ? $columnLastTpl : $columnTpl;
 //ID сессии Ditto
@@ -30,42 +33,63 @@ $dittoId = isset($dittoId) ? $dittoId.'_' : '';
 //Получаем необходимые результаты дитто
 $dittoRes = $modx->getPlaceholder($dittoId.'ditto_resource');
 
+//Всего строк
+$rowsTotal = count($dittoRes);
+
 //Если что-то есть
-if (count($dittoRes) > 0){
-	$res = array();
+if ($rowsTotal > 0){
+	//Количество элементов в колонке (общее количество элементов / количество колонок) 
+	$elementsInColumnNumber = ceil($rowsTotal / $columnsNumber);
 	
-	//Пробегаемся по результатам
-	foreach ($dittoRes as $key => $val){
-		//Запоминаем уже готовые отпаршенные значения
-		$res[] = $modx->getPlaceholder($dittoId.'item['.$key.']');
-	}
-	
-	//Всего строк
-	$rowsTotal = count($res);
-	//Если задано минимальное количество строк
-	if($rowsMin){
-		//Количество столбцов при минимальном количестве строк
+	//Если задано минимальное количество строк в колонке
+	if ($rowsMin){
+		//Количество колонок при минимальном количестве строк
 		$colsWithRowMin = ceil($rowsTotal / $rowsMin);
 	
 		//Если это количество меньше заданного
-		if($colsWithRowMin < $columnsNumber){
-			//Тогда количество столбцов будет меньше заданного (логика)
+		if ($colsWithRowMin < $columnsNumber){
+			//Тогда элементов в колонке будет меньше заданного (логика)
 			$elementsInColumnNumber = $rowsMin;
+			//И колонок тоже
+			$columnsNumber = $colsWithRowMin;
 		}
 	}
-
-	$elementsInColumnNumber = isset($elementsInColumnNumber)? $elementsInColumnNumber: ceil($rowsTotal/$columnsNumber);
-	//Разбиваем массив на части (размер каждого = количество элементов/количество колонок), сохраняя оригинальную нумерацию
-	$res = array_chunk($res, ceil($rowsTotal / $columnsNumber));
+	
+	//Если сортировка по строкам
+	if ($orderBy == 'row'){
+		$res = array_fill(0, $columnsNumber, array());
+		
+		$i = 0;
+		
+		//Пробегаемся по результатам
+		foreach ($dittoRes as $key => $val){
+			//Запоминаем уже готовые отпаршенные значения в нужную колонку
+			$res[$i][] = $modx->getPlaceholder($dittoId.'item['.$key.']');
+			
+			$i++;
+			if ($i == $columnsNumber){$i = 0;}
+		}
+	//В противном случае по колонкам
+	}else{
+		$res = array();
+	
+		//Пробегаемся по результатам
+		foreach ($dittoRes as $key => $val){
+			//Запоминаем уже готовые отпаршенные значения
+			$res[] = $modx->getPlaceholder($dittoId.'item['.$key.']');
+		}
+		
+		//Просто разбиваем массив на части, сохраняя оригинальную нумерацию
+		$res = array_chunk($res, $elementsInColumnNumber);
+	}
 	
 	$result = '';
 	$i = 0;
-	$len = count($res);
 	
 	//Перебираем колонки
-	while ($i < $len){
+	while ($i < $columnsNumber){
 		//Выбираем нужный шаблон
-		if ($i == $len - 1){
+		if ($i == $columnsNumber - 1){
 			$tpl = $columnLastTpl;
 		}else{
 			$tpl = $columnTpl;
