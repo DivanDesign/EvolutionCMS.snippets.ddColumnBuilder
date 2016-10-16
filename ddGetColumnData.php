cdff6e2d4bd63ecc104592b2046e80c2c49fd096
Ilyas Ronef <ilyas@divandesign.biz>
2016-10-16 20:14:33 +0500

    + Added support of “@CODE:” keyword prefix in the snippet templates. * Attention! The snippet now requires MODXEvo >= 1.1.

--- ddGetColumnData.php
+++ ddGetColumnData.php
@@ -8,20 +8,24 @@
  * @note Сниппет берёт результаты Ditto (2.1) из плэйсхолдера, так что перед его вызовом необходимо вызывать Ditto с параметром «save» == 3.
  * 
  * @uses PHP >= 5.4.
+ * @uses MODXEvo >= 1.1.
  * @uses The library modx.ddTools 0.15.4.
  * 
  * @param $columnsNumber {integer} — Количество колонок. Default: 1.
  * @param $rowsMin {integer} — Минимальное количество строк в одной колонке (0 — любое). Default: 0.
  * @param $orderBy {'column'|'row'} — Порядок элементов: 'column' — сначала заполняется первая колонка, потом вторая и т.д. ([[1, 2, 3], [4, 5, 6], [7, 8, 9]]); 'row' — элементы располагаются по срокам ([[1, 4, 7], [2, 5, 8], [3, 6, 9]]). Default: 'column'.
- * @param $columnTpl {string_chunkName} — Шаблон колонки. Доступные плэйсхолдеры: [+rows+], [+columnNumber+] (порядковый номер колонки). @required
- * @param $columnLastTpl {string_chunkName} — Шаблон последней колонки. Доступные плэйсхолдеры: [+rows+]. Default: = $columnTpl.
- * @param $outerTpl {string_chunkName} — Шаблон внешней обёртки. Доступные плэйсхолдеры: [+result+] (непосредственно результат), [+columnsNumber+] (фактическое количество колонок). Default: —.
+ * @param $columnTpl {string_chunkName|string} — Шаблон колонки (чанк или строка, начинающаяся с «@CODE:»). Доступные плэйсхолдеры: [+rows+], [+columnNumber+] (порядковый номер колонки). @required
+ * @param $columnLastTpl {string_chunkName|string} — Шаблон последней колонки (чанк или строка, начинающаяся с «@CODE:»). Доступные плэйсхолдеры: [+rows+]. Default: = $columnTpl.
+ * @param $outerTpl {string_chunkName|string} — Шаблон внешней обёртки (чанк или строка, начинающаяся с «@CODE:»). Доступные плэйсхолдеры: [+result+] (непосредственно результат), [+columnsNumber+] (фактическое количество колонок). Default: '@CODE:[+result+]'.
  * @param $placeholders {string_queryString} — Additional data as query string {@link https://en.wikipedia.org/wiki/Query_string } has to be passed into the result string. E. g. “pladeholder1=value1&pagetitle=My awesome pagetitle!”. Arrays are supported too: “some[a]=one&some[b]=two” => “[+some.a+]”, “[+some.b+]”; “some[]=one&some[]=two” => “[+some.0+]”, “[some.1]”. Default: ''.
  * @param $source {string|'ditto'} — Плэйсходлер (элемент массива «$modx->placeholders»), содержащий одномерный массив со строками исходных данных. Default: 'ditto'.
  * @param $dittoId {integer} — Уникальный ID сессии Ditto. Default: ''.
  * 
  * @copyright 2010–2016 DivanDesign {@link http://www.DivanDesign.biz }
  */
+
+//Подключаем modx.ddTools
+require_once $modx->getConfig('base_path').'assets/libs/ddTools/modx.ddtools.class.php';
 
 //Количество колонок
 $columnsNumber = (isset($columnsNumber) && is_numeric($columnsNumber) && $columnsNumber > 0) ? $columnsNumber : 1;
@@ -126,19 +130,19 @@ if ($rowsTotal > 0){
 		}
 		
 		//Парсим колонку
-		$result .= $modx->parseChunk($tpl, [
+		$result .= ddTools::parseText($modx->getTpl($tpl), [
 			'rows' => implode('', $res[$i]),
 			//Порядковый номер колонки
 			'columnNumber' => $i + 1
-		],'[+','+]');
+		]);
 		$i++;
 	}
 	
 	if (isset($outerTpl)){
-		$result = $modx->parseChunk($outerTpl, [
+		$result = ddTools::parseText($modx->getTpl($outerTpl), [
 			'result' => $result,
 			'columnsNumber' => $columnsNumber
-		], '[+', '+]');
+		]);
 	}
 	
 	//Если переданы дополнительные данные
@@ -147,9 +151,6 @@ if ($rowsTotal > 0){
 		parse_str($placeholders, $placeholders);
 		//Корректно инициализируем при необходимости
 		if (is_array($placeholders)){
-			//Подключаем modx.ddTools
-			require_once $modx->getConfig('base_path').'assets/libs/ddTools/modx.ddtools.class.php';
-			
 			//Unfold for arrays support (e. g. “some[a]=one&some[b]=two” => “[+some.a+]”, “[+some.b+]”; “some[]=one&some[]=two” => “[+some.0+]”, “[some.1]”)
 			$placeholders = ddTools::unfoldArray($placeholders);
 			
