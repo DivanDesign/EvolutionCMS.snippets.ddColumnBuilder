75e007cc0a94933c72a613bf94558f118b20baff
Ilyas Ronef <ilyas@divandesign.biz>
2017-07-06 16:10:40 +0500

    * Snippet is no longer works with MODX placeholders, instead just pass items glued by “source_itemsDelimiter” through the “source_items” parameter. - The “dittoId” parameter was removed. * The following parameters were renamed: 	* “source” → “source_items”. 	* “sourceDelimiter” → “source_itemsDelimiter”. 	* “rowsMin” → “minItemsInColumn”. 	* “orderBy” → “orderItemsBy”. 	* “columnTpl” → “tpls_column”. 	* “columnLastTpl” → “tpls_columnLast”. 	* “outerTpl” → “tpls_outer”. * The “[+rows+]” placeholder was renamed as “[+items+]”. * Attention! Backward compatibility is broken.

--- ddGetColumnData.php
+++ ddGetColumnData.php
@@ -11,81 +11,51 @@
  * @uses MODXEvo >= 1.1.
  * @uses MODXEvo.library.ddTools >= 0.15.4.
  * 
+ * @param $source_items {string} — Source items. @required
+ * @param $source_itemsDelimiter {string} — Source items delimiter. Default: '<!--ddGetColumnData-->'.
  * @param $columnsNumber {integer} — Количество колонок. Default: 1.
- * @param $rowsMin {integer} — Минимальное количество строк в одной колонке (0 — любое). Default: 0.
- * @param $orderBy {'column'|'row'} — Порядок элементов: 'column' — сначала заполняется первая колонка, потом вторая и т.д. ([[1, 2, 3], [4, 5, 6], [7, 8, 9]]); 'row' — элементы располагаются по срокам ([[1, 4, 7], [2, 5, 8], [3, 6, 9]]). Default: 'column'.
- * @param $columnTpl {string_chunkName|string} — Шаблон колонки (чанк или строка, начинающаяся с «@CODE:»). Доступные плэйсхолдеры: [+rows+], [+columnNumber+] (порядковый номер колонки). @required
- * @param $columnLastTpl {string_chunkName|string} — Шаблон последней колонки (чанк или строка, начинающаяся с «@CODE:»). Доступные плэйсхолдеры: [+rows+]. Default: = $columnTpl.
- * @param $outerTpl {string_chunkName|string} — Шаблон внешней обёртки (чанк или строка, начинающаяся с «@CODE:»). Доступные плэйсхолдеры: [+result+] (непосредственно результат), [+columnsNumber+] (фактическое количество колонок). Default: '@CODE:[+result+]'.
+ * @param $minItemsInColumn {integer} — Минимальное количество элементов в одной колонке (0 — любое). Default: 0.
+ * @param $orderItemsBy {'column'|'row'} — Порядок элементов: 'column' — сначала заполняется первая колонка, потом вторая и т.д. ([[1, 2, 3], [4, 5, 6], [7, 8, 9]]); 'row' — элементы располагаются по срокам ([[1, 4, 7], [2, 5, 8], [3, 6, 9]]). Default: 'column'.
+ * @param $tpls_column {string_chunkName|string} — Шаблон колонки (чанк или строка, начинающаяся с «@CODE:»). Доступные плэйсхолдеры: [+items+], [+columnNumber+] (порядковый номер колонки). @required
+ * @param $tpls_columnLast {string_chunkName|string} — Шаблон последней колонки (чанк или строка, начинающаяся с «@CODE:»). Доступные плэйсхолдеры: [+items+]. Default: = $tpls_column.
+ * @param $tpls_outer {string_chunkName|string} — Шаблон внешней обёртки (чанк или строка, начинающаяся с «@CODE:»). Доступные плэйсхолдеры: [+result+] (непосредственно результат), [+columnsNumber+] (фактическое количество колонок). Default: '@CODE:[+result+]'.
  * @param $placeholders {string_queryString} — Additional data as query string {@link https://en.wikipedia.org/wiki/Query_string } has to be passed into the result string. E. g. “pladeholder1=value1&pagetitle=My awesome pagetitle!”. Arrays are supported too: “some[a]=one&some[b]=two” => “[+some.a+]”, “[+some.b+]”; “some[]=one&some[]=two” => “[+some.0+]”, “[some.1]”. Default: ''.
- * @param $source {string|'ditto'} — Плэйсходлер (элемент массива «$modx->placeholders»), содержащий одномерный массив со строками исходных данных. Default: 'ditto'.
- * @param $sourceDelimiter {string} — Разделитель поступившей строки. Default: ''.
- * @param $dittoId {integer} — Уникальный ID сессии Ditto. Default: ''.
  * 
  * @copyright 2010–2016 DivanDesign {@link http://www.DivanDesign.biz }
  */
 
-//Подключаем modx.ddTools
+//Include MODXEvo.library.ddTools
 require_once $modx->getConfig('base_path').'assets/libs/ddTools/modx.ddtools.class.php';
+
+$source_itemsDelimiter = isset($source_itemsDelimiter) ? $source_itemsDelimiter : '<!--ddGetColumnData-->';
+$source_items = isset($source_items) ? explode($source_itemsDelimiter, $source_items) : [];
 
 //Количество колонок
 $columnsNumber = (isset($columnsNumber) && is_numeric($columnsNumber) && $columnsNumber > 0) ? $columnsNumber : 1;
 //Минимальное количество строк
-$rowsMin = isset($rowsMin) ? intval($rowsMin) : 0;
+$minItemsInColumn = isset($minItemsInColumn) ? intval($minItemsInColumn) : 0;
 //Сортировка между колонками
 $orderBy = isset($orderBy) ? $orderBy : 'column';
 //Если шаблон последней колонки, не задан — будет как и все
-$columnLastTpl = isset($columnLastTpl) ? $columnLastTpl : $columnTpl;
-//Источник
-$source = isset($source) ? $source : 'ditto';
-
-$rowsTotal = 0;
-
-if (!isset($sourceDelimiter)){
-	//TODO: Избавиться от этого, в топку модх плейсхолдеры, надо просто сюда пихать массив
-	//TODO: Сменить название сниппета
-	//TODO: Выпустить новую версию
-	if (strtolower($source) == 'ditto'){
-		//ID сессии Ditto
-		$dittoId = isset($dittoId) ? $dittoId.'_' : '';
-		
-		//Получаем необходимые результаты дитто
-		if ($dittoRes = $modx->getPlaceholder($dittoId.'ditto_resource')){
-			$source = [];
-			
-			foreach ($dittoRes as $key => $val){
-				$source[] = $modx->getPlaceholder($dittoId.'item['.$key.']');
-			}
-		}
-	}else{
-		$source = $modx->getPlaceholder($source);
-		
-		if (!is_array($source)){
-			$source = [];
-		}
-	}
-}else{
-	$source = explode($sourceDelimiter, $source);
-}
-
+$tpls_columnLast = isset($tpls_columnLast) ? $tpls_columnLast : $tpls_column;
 
 //Всего строк
-$rowsTotal = count($source);
+$itemsTotal = count($source_items);
 
 //Если что-то есть
-if ($rowsTotal > 0){
+if ($itemsTotal > 0){
 	//Количество элементов в колонке (общее количество элементов / количество колонок) 
-	$elementsInColumnNumber = ceil($rowsTotal / $columnsNumber);
+	$elementsInColumnNumber = ceil($itemsTotal / $columnsNumber);
 	
 	//Если задано минимальное количество строк в колонке
-	if ($rowsMin){
+	if ($minItemsInColumn){
 		//Количество колонок при минимальном количестве строк
-		$colsWithRowMin = ceil($rowsTotal / $rowsMin);
+		$colsWithRowMin = ceil($itemsTotal / $minItemsInColumn);
 		
 		//Если это количество меньше заданного
 		if ($colsWithRowMin < $columnsNumber){
 			//Тогда элементов в колонке будет меньше заданного (логика)
-			$elementsInColumnNumber = $rowsMin;
+			$elementsInColumnNumber = $minItemsInColumn;
 			//И колонок тоже
 			$columnsNumber = $colsWithRowMin;
 		}
@@ -98,7 +68,7 @@ if ($rowsTotal > 0){
 		$i = 0;
 		
 		//Пробегаемся по результатам
-		foreach ($source as $val){
+		foreach ($source_items as $val){
 			//Запоминаем уже готовые отпаршенные значения в нужную колонку
 			$res[$i][] = $val;
 			
@@ -112,13 +82,13 @@ if ($rowsTotal > 0){
 		//Проходка по кол-ву колонок-1
 		for ($i = 1; $i < $columnsNumber; $i++){ 
 			//Заполняем колонку нужным кол-вом
-			$res[] = array_splice($source, 0, $elementsInColumnNumber);
+			$res[] = array_splice($source_items, 0, $elementsInColumnNumber);
 			//Пересчет кол-ва в колонке для оставшегося кол-ва элементов и колонок
-			$elementsInColumnNumber = ceil(count($source) / ($columnsNumber - $i));
+			$elementsInColumnNumber = ceil(count($source_items) / ($columnsNumber - $i));
 		}
 		
 		//Последняя колонка с остатком
-		$res[] = $source;
+		$res[] = $source_items;
 	}
 	
 	$result = '';
@@ -133,22 +103,22 @@ if ($rowsTotal > 0){
 	while ($i < $columnsNumber){
 		//Выбираем нужный шаблон (если колонка последняя, но не единственная)
 		if ($columnsNumber > 1 && $i == $columnsNumber - 1){
-			$tpl = $columnLastTpl;
+			$tpl = $tpls_columnLast;
 		}else{
-			$tpl = $columnTpl;
+			$tpl = $tpls_column;
 		}
 		
 		//Парсим колонку
 		$result .= ddTools::parseText($modx->getTpl($tpl), [
-			'rows' => implode('', $res[$i]),
+			'items' => implode('', $res[$i]),
 			//Порядковый номер колонки
 			'columnNumber' => $i + 1
 		]);
 		$i++;
 	}
 	
-	if (isset($outerTpl)){
-		$result = ddTools::parseText($modx->getTpl($outerTpl), [
+	if (isset($tpls_outer)){
+		$result = ddTools::parseText($modx->getTpl($tpls_outer), [
 			'result' => $result,
 			'columnsNumber' => $columnsNumber
 		]);
